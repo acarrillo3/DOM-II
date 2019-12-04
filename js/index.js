f92734c64d823c8c007b81508e40084109a980fd
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-03 19:17:19 -0500

    completed 3 out of 10

--- js/index.js
+++ js/index.js
@@ -1,1 +1,33 @@
 // Your code goes here
+// var glow = $(".logo-heading");
+// setInterval(function(){
+//     glow.toggleClass('glow');
+// }, 1000);
+
+// 1. mouse events
+
+const logoImg = document.querySelector(".logo-heading")
+console.log(logoImg);
+
+logoImg.addEventListener("mouseenter", () => {
+    logoImg.style.transform = "scale(1.2)";
+    logoImg.style.transition = "transform 0.3s"
+  })
+  logoImg.addEventListener("mouseleave", () => {
+    logoImg.style.transform = "scale(1)"
+  });
+
+//2. on click event
+document.querySelectorAll(".btn").forEach(el => {
+    el.addEventListener("click", function() {
+      el.style.color= "gold";
+    })
+  })
+
+  //3. double click event
+
+const colorNav = document.querySelector('.nav-link');
+colorNav.addEventListener("dblclick", () => {
+    colorNav.style.color= "orange";
+})
+
