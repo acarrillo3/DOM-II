d06eedde1c3a02f47de7ceda1ca74771eb654f07
Ana Zoe <50473626+acarrillo3@users.noreply.github.com>
2019-12-03 21:59:41 -0500

    had fun finishing up mvp

--- js/index.js
+++ js/index.js
@@ -4,30 +4,76 @@
 //     glow.toggleClass('glow');
 // }, 1000);
 
-// 1. mouse events
+
 
 const logoImg = document.querySelector(".logo-heading")
 console.log(logoImg);
-
+//1. mouseenter 
 logoImg.addEventListener("mouseenter", () => {
-    logoImg.style.transform = "scale(1.2)";
+    logoImg.style.transform = "scale(2)";
     logoImg.style.transition = "transform 0.3s"
   })
+  //2.mouse leave
   logoImg.addEventListener("mouseleave", () => {
     logoImg.style.transform = "scale(1)"
   });
 
-//2. on click event
+//3. on click event
 document.querySelectorAll(".btn").forEach(el => {
     el.addEventListener("click", function() {
       el.style.color= "gold";
     })
   })
 
-  //3. double click event
-
-const colorNav = document.querySelector('.nav-link');
-colorNav.addEventListener("dblclick", () => {
-    colorNav.style.color= "orange";
+//4. double click event
+const colorText = document.querySelector('body');
+colorText.addEventListener("dblclick", () => {
+    colorText.style.color= "orange";
+    colorText.style.background= "black";
 })
 
+//5. keydown
+//this checks wha key pressed on console
+window.addEventListener('keydown', event => {
+    //key pressed
+    console.log(event.key)
+})
+
+//6. auxclick
+const rotImg = document.querySelector(".img-content")
+
+rotImg.addEventListener("auxclick", () => {
+    rotImg.style.transform="rotate(30deg)"
+})
+
+//7. load
+//this also prints on the console
+window.addEventListener('load', e => {
+    console.log("You are now connected to the mainframe.");
+})
+
+// 8.Wheel
+const bod = document.querySelector('body');
+const navHeader = document.querySelector('.main-navigation');
+console.log(navHeader);
+bod.addEventListener('wheel', () => {
+    navHeader.style.position="absolute";
+    navHeader.style.top="150%";
+    navHeader.style.transform="rotate(0.9turn)";
+})
+
+//9. Drag
+const lastImg = document.querySelector('.content-destination img');
+lastImg.addEventListener('drag', () => {
+    lastImg.style.width="100%";
+    navHeader.style.transform="";
+})
+
+//10.Mouseover
+const h2M = document.querySelector('.text-content h2')
+h2M.addEventListener('mouseover', () => {
+    navHeader.style.position="fixed";
+    navHeader.style.top="0";
+    colorText.style.color= "hotpink";
+
+})
